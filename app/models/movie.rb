08788d36d3e8acd972a77161b151b08337c42621
Yanming Tse <tseyan113@gmail.com>
2022-08-25 15:26:51 +0100

    model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  validates :poster_url, presence: true
-  validates :rating, presence: true
-
-  has_many :bookmarkss, dependent: :destroy
 end
--- db/migrate/20220825120345_create_bookmarks.rb
+++
@@ -1,9 +0,0 @@
-class CreateBookmarks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :bookmarks do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
